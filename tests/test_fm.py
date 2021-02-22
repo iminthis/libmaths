3fc95ba8ca38a07963a95892cda8c2916b948490
Simple <69445064+Simple2006@users.noreply.github.com>
2021-02-22 00:28:44 -0800

    Update test_fm.py

--- tests/test_fm.py
+++ tests/test_fm.py
@@ -1,5 +1,65 @@
-from funcMath import fm
+from funcMath import fm, fmgraph
 
 def test_sigmoid():
 
-    print(myfunctions.sigmoid(0))
+    print(fm.sigmoid(0))
+
+def test_sigmoid1():
+    
+    fmgraph.sigmoid()
+
+def test_relu():
+
+    print(fm.relu(5))
+
+def test_relu1():
+
+    fmgraph.relu()
+
+def test_constant():
+
+    fmgraph.constant(100)
+
+def test_linear():
+
+    fmgraph.linear(5, 2)
+
+def test_psflinear():
+
+    fmgraph.psflinear(6, 0.5, 20)
+
+def test_quadratic():
+
+    fmgraph.quadratic(1, 0, 0)
+
+def test_vtquadratic():
+
+    fmgraph.vtquadratic(2, -6, 20)
+
+def test_cubic():
+
+    fmgraph.cubic(2, -3, -3, -35)
+
+def test_trigsin():
+
+    fmgraph.trigsin(1, 0.5)
+
+def test_trigcos():
+
+    fmgraph.trigcos(2, 1)
+
+def test_trigtan():
+
+    fmgraph.trigtan(5, 2)
+
+def test_quartic():
+
+    fmgraph.quartic(10, -10, 5, 5, -150)
+
+def test_quintic():
+
+    fmgraph.quintic(1, -5, 5, 5, -6, -1)
+
+def test_sextic():
+
+    fmgraph.sextic(2, 4, 0, 2, 10, 4, 2)
