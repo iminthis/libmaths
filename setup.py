97fce4d9569e72c941783e93c02faf4f9fcd73e6
Simple <vinayven01@gmail.com>
2021-02-25 18:05:37 -0800

    update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as reqs:
 
 setuptools.setup(
     name="libmaths",
-    version="0.1.7",
+    version="0.1.8",
     author="Vinay Venkatesh",
     author_email="vinayven01@gmail.com",
     description="LibMaths is a Python library created to assist programmers with complex mathematical functions as well as for learning more about Python.",
